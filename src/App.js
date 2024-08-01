52b5735f2c00b2125819d26120db32e28e4e2ea2
NathanHM <NathanHM16@gmail.com>
2024-08-01 19:05:56 +0100

    Setup initial routing features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Root from './Root';
+import Callback from './Callback';
+
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Root />}>
+    <Route path='callback' element={<Callback />} />
+  </Route>
+))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <RouterProvider router={appRouter} />
+
   );
 }
 
---
+++ src/Callback.js
@@ -0,0 +1,9 @@
+import { Navigate } from 'react-router-dom';
+
+export default function Callback() {
+    return (
+
+        <Navigate to='/' />
+
+    )
+}
